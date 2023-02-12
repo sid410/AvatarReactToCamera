fde0c79afae095ab0820bb46708ad9adc96331c2
kaguyasamaproject <85621840+kaguyasamaproject@users.noreply.github.com>
2023-02-12 21:13:52 +0900

    Demo

--- AvatarReactUnity/Assets/Scripts/SystemFlow/LightManager.cs
+++ AvatarReactUnity/Assets/Scripts/SystemFlow/LightManager.cs
@@ -9,11 +9,33 @@ public class LightManager : MonoBehaviour
     private Color lightColor;
     private float colorValue;
     private string KaguyaState;
+    private GameObject[] lightObjects;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.gameObject.GetComponent<Light>().color = new Color(255, 255, 100);
+        string[] lightObjectName = new string[14] {
+            "Point.001",
+            "Point.002",
+            "Point.003",
+            "Point.004",
+            "Point.005",
+            "Point.006",
+            "Point.007",
+            "Point.008",
+            "Point.009",
+            "Point.010",
+            "Point.011",
+            "Point.012",
+            "Point.013",
+            "Point.014"};
+
+        lightObjects = new GameObject[14];
+        for (int i = 0; i < lightObjectName.Length; i++)
+        {
+            lightObjects[i] = GameObject.Find(lightObjectName[i]);
+            lightObjects[i].GetComponent<Light>().color = new Color(1.0f, 1.0f, 0.40f, 1.0f);
+        }
         colorValue = 0.0f;
     }
 
@@ -22,10 +44,11 @@ public class LightManager : MonoBehaviour
     {
         KaguyaState = Kaguya.GetComponent<TsunderadorMenu>().state.getState();
 
-        if (KaguyaState == "TsunderadorSecond" && colorValue <= 155.0f)
+        if (KaguyaState == "TsunderadorSecond" && colorValue <= 0.60f)
         {
-            this.gameObject.GetComponent<Light>().color = new Color(255, 255 - colorValue, 100 + colorValue);
-            colorValue += 0.1f;
+            for (int i=0; i < lightObjects.Length; i++)
+                lightObjects[i].GetComponent<Light>().color = new Color(1.0f, 1.0f - colorValue, 0.40f + colorValue);
+            colorValue += 0.001f;
         }
     }
 }
--- AvatarReactUnity/Assets/Scripts/SystemFlow/TsunderadorMenu.cs
+++ AvatarReactUnity/Assets/Scripts/SystemFlow/TsunderadorMenu.cs
@@ -62,7 +62,8 @@ public class TsunderadorMenu : MonoBehaviour
 
     bool MotionTriger()
     {
-        if (Input.GetKey(KeyCode.Space)) return true;
+        if (state.getState() == "Start" && Input.GetKey(KeyCode.LeftShift)) return true;
+        if (state.getState() == "TsunderadorFirst" && Input.GetKey(KeyCode.LeftControl)) return true;
         else return false;
 
         //if (state.getState() == "Start" && inputMessage == "state1") return true;
